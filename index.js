6567b8ec09e6136956d82957a1654dff16869cda
Vakkas Emre Yıldız <72315833+vakkas1234@users.noreply.github.com>
2024-09-04 13:21:03 +0300

    everything is okay

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-function click1(){
-    document.getElementById("1").style.visibility = "hidden"
-document.getElementById("2").innerHTML = `
-<p> Welcome to new page </p>
-<button onclick="emre()">Return to the home page </button>
-`
 
+const maincontent = document.getElementById("3").innerHTML
+window.onload = function(){
+    const savedcontent = localStorage.getItem("content")
+    const isInitial = localStorage.getItem("isInitial")
+    if(savedcontent){
+    document.getElementById("2").innerHTML = savedcontent
+    if(isInitial === "false"){
+        document.getElementById("1").style.visibility = "hidden"
+       
+    }
+
+    
+    }
+    else{
+         const initialcontent = document.getElementById("2").innerHTML
+         localStorage.setItem("content", initialcontent)
+         localStorage.setItem("isInitial" , "true")
+    }
 }
+function click1(){
 
-function emre(){
-    window.location.href = "index.html"
-   
+    const newcontent = `
+    <p>Welcome to new page </p>
+    <a href="#" onclick="click2()">go back </a>
+
+    `
+    document.getElementById("2").innerHTML = newcontent
+    document.getElementById("1").style.visibility  = "hidden"
+    localStorage.setItem("content", newcontent)
+    localStorage.setItem("isInitial" , "false")
+}
+function click2(){
+    document.getElementById("2").innerHTML = maincontent
+    document.getElementById("1").style.visibility = "visible"
+    localStorage.setItem("content" , maincontent)
+    localStorage.setItem("isInitial" , "true")
 }
